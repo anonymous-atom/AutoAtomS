c80c58b0402954a98079211fd81f65b643f46ad5
Karun <mailto.karun.py@gmail.com>
2023-03-14 12:29:26 +0530

    UPdates HTML Templates

--- app.py
+++ app.py
@@ -1,39 +1,28 @@
-import requests
 from flask import Flask, render_template, request
 import openai
 import os
 
-
-openai.api_key = "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"
+openai.api_key = "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"
 app = Flask(__name__, template_folder='/home/karun/PycharmProjects/AutoAtomS/templates')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.template_folder = os.path.abspath('templates')
 
+def generate_response(prompt):
+    completion = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt},
+            ],
+    )
+    return completion["choices"][0]["message"]["content"]
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    result = ''
     if request.method == 'POST':
-        # Get the user's input from the form
-        user_input = request.form['user_input']
-
-        # Make the API call
-        prompt = f'User input: {user_input}\nAI response:'
-
-        completion = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo",
-                messages = [
-                    {"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": user_input},
-                ],
-        )
-
-        # Extract the response text from the API response
-        result = completion["choices"][0]["message"]["content"]
-
-        # Render the result to the user
-        return render_template('index.html', result=result)
-
-    # If the request method is GET, display the input form
+        prompt = request.form['prompt']
+        response = generate_response(prompt)
+        return render_template('output.html', prompt=prompt, response=response)
     return render_template('index.html')
 
 if __name__ == '__main__':
